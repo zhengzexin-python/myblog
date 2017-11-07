4e41fbf5a518c8e1fef5bdba1357a2bde196d1aa
zhengzexin-python <zzz.kagari@gmail.com>
2017-11-07 22:55:27 +0800

    创建博客数据库模型

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+# 分类
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+# 标签
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+# 文章
+class Post(models.Model):
+    title = models.CharField(max_length=100)  # 标题
+    body = models.TextField()  # 文章内容
+    create_time = models.DateTimeField()  # 创建时间
+    modified_time = models.DateTimeField()  # 最后修改时间
+    excerpt = models.CharField(max_length=100, blank=True)  # 文章摘要 允许为空
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)  # 多对多关系 允许标签为空
+    author = models.ForeignKey(User)  # 文章作者
