4af9cc22dbcec1fc241f2fac8a90e912278a6ad4
zhengzexin-python <zzz.kagari@gmail.com>
2017-11-12 19:01:21 +0800

    markdown语法支持

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from blog.models import Post
+import markdown
 
 
 def index(request):
@@ -11,5 +12,10 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-
+    # 使用markdown进行渲染
+    post.body = markdown.markdown(post.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',  # 语法高亮拓展
+        'markdown.extensions.toc',
+    ])
     return render(request, 'blog/detail.html', context={'post': post})
