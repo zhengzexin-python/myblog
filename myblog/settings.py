859294ca814f7145963c9c038c599ff5587b4f9a
zhengzexin-python <zzz.kagari@gmail.com>
2017-11-14 23:43:45 +0800

    为部署到服务器上修改配置

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't&!#a_-%r&bd#by36u(dc&$jq*xpfcs&d(r&d2s9tpe4t^y98k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.zexin.site']
 
 # Application definition
 
@@ -116,3 +116,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
