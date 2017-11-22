c02b28a50cf5603c9b839830d1385035dd1ebc31
zhengzexin-python <zzz.kagari@gmail.com>
2017-11-22 11:36:54 +0800

    Signed-off-by: zhengzexin-python <zzz.kagari@gmail.com>

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't&!#a_-%r&bd#by36u(dc&$jq*xpfcs&d(r&d2s9tpe4t^y98k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.zexin.site']
 
