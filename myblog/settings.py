94f7a33f2ef19a11b13773cae2465a4bd1d900b7
zhengzexin-python <zzz.kagari@gmail.com>
2018-03-12 17:41:19 +0800

    功能修改

--- myblog/settings.py
+++ myblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't&!#a_-%r&bd#by36u(dc&$jq*xpfcs&d(r&d2s9tpe4t^y98k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.zexin.site']
 
@@ -75,11 +75,11 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'postgresql': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myblog',
         'USER': 'postgres',
